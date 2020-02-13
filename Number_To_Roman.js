1648ebda781f0171ec8ae4f7632c8751d6be709c
CatanaRaulAndrei <raul19@windowslive.com>
2020-02-13 18:52:07 +0200

    Update Number_To_Roman.js

--- Number_To_Roman.js
+++ Number_To_Roman.js
@@ -1,12 +1,13 @@
 function convertToRoman(num){
-    let roman_to_number = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
-    for(i in roman_to_number){
+    let roman_to_number = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+    let roman = '';
+    for(let i in roman_to_number){
              while ( num >= roman_to_number[i] ) {
-                    roman += i;
-                    num -= roman_to_number[i];
+                    roman = roman + i;
+                    num = num - roman_to_number[i];
              }
     } 
 return roman;
 }
 
-convertToRoman(2); // II
+document.write(convertToRoman(2)); // II
