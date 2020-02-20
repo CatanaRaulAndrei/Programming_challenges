9ba3f32b854df5bda2dc42f9b85618b5d81358a5
CatanaRaulAndrei <raul19@windowslive.com>
2020-02-20 19:12:18 +0200

    Update square_pattern.c

--- square_pattern.c
+++ square_pattern.c
@@ -2,6 +2,22 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+/*
+7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 6 6 6 6 6 6 6 6 6 6 6 7 
+7 6 5 5 5 5 5 5 5 5 5 6 7 
+7 6 5 4 4 4 4 4 4 4 5 6 7 
+7 6 5 4 3 3 3 3 3 4 5 6 7 
+7 6 5 4 3 2 2 2 3 4 5 6 7 
+7 6 5 4 3 2 1 2 3 4 5 6 7 
+7 6 5 4 3 2 2 2 3 4 5 6 7 
+7 6 5 4 3 3 3 3 3 4 5 6 7 
+7 6 5 4 4 4 4 4 4 4 5 6 7 
+7 6 5 5 5 5 5 5 5 5 5 6 7 
+7 6 6 6 6 6 6 6 6 6 6 6 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 
+*/
+
 #define minimum(firstNumber, secondNumber) (firstNumber < secondNumber ? firstNumber : secondNumber);
 void find_pattern(int n){
     int length = 2*n - 1, min=0;
